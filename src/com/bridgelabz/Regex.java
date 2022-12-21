ca62260a9219dc28e5a2438d3384e2443ee7f559
9663461085 <ratansollapure16310@gmail.com>
2022-12-21 14:22:18 +0530

    Added:Checked for first name validation

--- src/com/bridgelabz/Regex.java
+++ src/com/bridgelabz/Regex.java
@@ -1,4 +1,9 @@
 package com.bridgelabz;
+
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /***
  * 
  * @author Admin
@@ -9,7 +14,22 @@ public class Regex {
 	 * 
 	 * @param args
 	 */
+	void displayFirstName() {
+		Scanner input = new Scanner(System.in);
+		System.out.println("Enter First_Name=");
+		String firstName = input.next();
+		String y = "[A-Z]{1}[a-z]{2}";
+		Pattern p = Pattern.compile(y);
+		Matcher m = p.matcher(firstName);
+		if (m.matches()) {
+			System.out.println("First Name Validate");
+		} else {
+			System.out.println("First Name Not  Validate");
+		}
+	}
+
 	public static void main(String[] args) {
-		System.out.println("Welcome to Regex Programm");
+		Regex display = new Regex();
+		display.displayFirstName();
 	}
 }
